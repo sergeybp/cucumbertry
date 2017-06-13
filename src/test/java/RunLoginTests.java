563ceba53b42fb553766fdecb299649f69dd317e
sergeybp <sergeybackpath@gmail.com>
2017-06-13 17:10:01 +0300

    fix

--- src/test/java/RunLoginTests.java
+++ src/test/java/RunLoginTests.java
@@ -1,18 +1,13 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-import org.testng.annotations.Test;
 
 /**
  * Created by sergeybp on 07.06.17.
  */
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "classpath:features/Login.features",
-        glue = "steps"
-)
-public class RunLoginTests { // NOSONAR SonarLint does not know about @RunWith(Cucumber.class)
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+@CucumberOptions (features ="classpath:features/Login.features",
+        glue={"steps"})
+public class RunLoginTests extends AbstractTestNGCucumberTests{
 }
-
-
